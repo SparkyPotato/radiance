4ec57fa6abf0e41120654ce4d4626d9947d06b40
SparkyPotato <noob.sparkypotato@gmail.com>
2024-10-09 02:18:38 +0100

    kinda fix fibers

--- crates/exec/src/fibers.rs
+++ crates/exec/src/fibers.rs
@@ -1,16 +1,8 @@
-use std::arch::asm;
-
-pub struct Fiber {
-	context: ThreadContext,
-	stack: *mut u8,
-}
-
-impl Fiber {
-	pub fn new(stack_base: *mut u8) {}
-}
+use std::{arch::naked_asm, marker::PhantomPinned, pin::Pin};
 
 #[repr(C)]
-struct ThreadContext {
+#[derive(Default)]
+pub struct Fiber {
 	rip: u64,
 	rsp: u64,
 	rbx: u64,
@@ -19,14 +11,49 @@ struct ThreadContext {
 	r13: u64,
 	r14: u64,
 	r15: u64,
+	_pin: PhantomPinned,
+}
+
+impl Fiber {
+	pub fn null() -> Self {
+		Self {
+			rip: 0,
+			rsp: 0,
+			rbx: 0,
+			rbp: 0,
+			r12: 0,
+			r13: 0,
+			r14: 0,
+			r15: 0,
+			_pin: PhantomPinned,
+		}
+	}
+
+	pub fn init(
+		self: &mut Pin<&mut Self>, stack: *mut u8, size: u64, exec: extern "sysv64" fn(*mut Self, *mut u8),
+		arg: *mut u8,
+	) {
+		let this = unsafe { &mut self.as_mut().get_unchecked_mut() };
+		this.rsp = stack as u64 + size;
+		this.rip = Self::internal_trampoline as _;
+		this.rbx = exec as _;
+		this.rbp = this.rsp;
+		this.r12 = this as *mut _ as _;
+		this.r13 = arg as _;
+	}
+
+	#[naked]
+	unsafe extern "sysv64" fn internal_trampoline() {
+		naked_asm!("mov rdi, r12", "mov rsi, r13", "jmp rbx",);
+	}
 }
 
 #[naked]
-unsafe extern "sysv64" fn swap_context(store: *mut ThreadContext, load: *mut ThreadContext) {
-	asm!(
-		"mov r8, [rsp + 8 * 0]",
+pub unsafe extern "sysv64" fn swap_context(store: *mut Fiber, load: *mut Fiber) {
+	naked_asm!(
+		"mov r8, [rsp]", // mov r8, return-addr
 		"mov [rdi + 8 * 0], r8",
-		"mov r8, [rdi + 8 * 1]",
+		"lea r8, [rsp + 8]", // r8 = rsp + 8 (whatever it was before the call)
 		"mov [rdi + 8 * 1], r8",
 		"mov [rdi + 8 * 2], rbx",
 		"mov [rdi + 8 * 3], rbp",
@@ -42,9 +69,27 @@ unsafe extern "sysv64" fn swap_context(store: *mut ThreadContext, load: *mut Thr
 		"mov r13, [rsi + 8 * 5]",
 		"mov r14, [rsi + 8 * 6]",
 		"mov r15, [rsi + 8 * 7]",
-		"push r8",
 		"xor eax, eax",
-		"ret",
-		options(noreturn)
+		"jmp r8",
 	);
+}
+
+#[test]
+fn can_swap_context() {
+	extern "sysv64" fn other_fn(us: *mut Fiber, arg: *mut u8) {
+		println!("hello from other fiber");
+		unsafe {
+			swap_context(us, arg as _);
+		}
+	}
+
+	let mut stack = vec![0; 4096];
+	let mut us = Fiber::null();
+	let mut other = Fiber::null();
+	let mut other = std::pin::pin!(other);
+	other.init(stack.as_mut_ptr(), stack.len() as u64, other_fn, &mut us as *mut _ as _);
+	unsafe {
+		swap_context(&mut us, other.get_unchecked_mut());
+	}
+	println!("we're back");
 }
